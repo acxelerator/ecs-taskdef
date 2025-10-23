bfec6c97bee8bf0cff91a6f345e1b47bb39a5afc
gsy0911 <yoshiki0911@gmail.com>
2025-10-23 12:59:48 +0900

    add Secrets

--- src/ecs_taskdef/domain/service/get_secrets.py
+++ src/ecs_taskdef/domain/service/get_secrets.py
@@ -2,6 +2,7 @@ import json
 
 import boto3
 from botocore.exceptions import ClientError
+from ecs_taskdef.domain.entity.container_definition import Secrets
 
 
 class SecretValue:
@@ -18,3 +19,10 @@ class SecretValue:
 
         secret = get_secret_value_response["SecretString"]
         return json.loads(secret)
+
+    def get_as_secrets(self, secrets_manager_arn: str) -> list[dict]:
+        secret_dict = self.get_from_secrets_manager(secret_name=secrets_manager_arn)
+        result = []
+        for k, v in secret_dict.items():
+            result.append(Secrets(name=k, valueFrom=secrets_manager_arn))
+        return result
